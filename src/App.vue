f938fb510860549c17c58ca1a8a43d216e40deb7
Calvin <cc.paparun@gmail.com>
2026-01-26 13:03:08 +0800

    refactor: 💡 Remove commented part of old Filter code

--- src/App.vue
+++ src/App.vue
@@ -110,22 +110,6 @@ onMounted(() => {
         :filterStatus="filterStatus"
         @clearFilters="clearFilters"
       />
-      <!-- <div
-        v-if="filterLevel || filterStatus"
-        class="my-4 p-3 bg-terminal-yellow/10 border border-terminal-yellow rounded"
-      >
-        <span class="text-terminal-yellow text-sm">
-          🔍 篩選中:
-          <span v-if="filterLevel" class="font-bold">{{ filterLevel }}</span>
-          <span v-if="filterStatus" class="font-bold">{{ filterStatus }}</span>
-          <button
-            @click="clearFilters"
-            class="ml-4 px-2 py-1 text-xs bg-terminal-yellow text-terminal-bg rounded hover:opacity-80"
-          >
-            清除篩選
-          </button>
-        </span>
-      </div> -->
 
       <!-- 控制按鈕 -->
       <LogControls />
